85666309acfe6778dfcb310526990a669139afcc
KUJ <deppll6239@gmail.com>
2024-09-13 17:38:04 +0900

    spring day17

---
+++ 6_spring/workspace/demo8/src/main/java/com/example/demo/service/BoardService.java
@@ -0,0 +1,21 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.demo.domain.BoardDTO;
+import com.example.demo.domain.Criteria;
+import com.example.demo.domain.FileDTO;
+
+public interface BoardService {
+	List<BoardDTO> getList(Criteria cri);
+	long getTotal(Criteria cri);
+	boolean regist(BoardDTO board, MultipartFile[] files) throws Exception;
+	BoardDTO getDetail(long boardnum);
+	long getLastNum(String userid);
+	void increaseReadCount(long boardnum);
+	boolean modify(BoardDTO board, MultipartFile[] files, String updateCnt) throws Exception;
+	boolean remove(long boardnum);
+	List<FileDTO> getFiles(long boardnum);
+}
---
+++ 6_spring/workspace/demo8/src/main/java/com/example/demo/service/ReplyServiceImpl.java
@@ -0,0 +1,67 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.Criteria;
+import com.example.demo.domain.ReplyDTO;
+import com.example.demo.domain.ReplyPageDTO;
+import com.example.demo.mapper.ReplyMapper;
+
+@Service
+public class ReplyServiceImpl implements ReplyService{
+	@Autowired
+	private ReplyMapper rmapper;
+	
+	@Override
+	public ReplyDTO regist(ReplyDTO reply) {
+		if(rmapper.insertReply(reply) == 1) {
+			return rmapper.getLastReply(reply.getUserid());
+		}
+		return null;
+	}
+	
+	@Override
+	public ReplyPageDTO getList(Criteria cri, long boardnum) {
+//		return new ReplyPageDTO(rmapper.getTotal(boardnum), rmapper.getList(cri,boardnum));
+		
+		long totalCnt = rmapper.getTotal(boardnum);
+		List<ReplyDTO> list = rmapper.getList(cri,boardnum);
+		
+		ReplyPageDTO dto = new ReplyPageDTO(totalCnt, list);
+		
+		return dto;
+	}
+	
+	@Override
+	public boolean remove(long replynum) {
+		return rmapper.deleteReply(replynum) == 1;
+	}
+	
+	@Override
+	public boolean modify(ReplyDTO reply) {
+		ReplyDTO rdto = rmapper.getDetail(reply.getReplynum());
+		if(rdto.getUserid().equals(reply.getUserid())) {
+			return rmapper.updateReply(reply) == 1;
+		}
+		return false;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
